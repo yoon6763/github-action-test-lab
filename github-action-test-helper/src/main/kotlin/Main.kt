6a2c7d8bbd3dfa03e9b4211cf224ab261761f41a
uknow-home <yoon6763@naver.com>
2023-09-20 01:56:39 +0900

    코드 수정

--- github-action-test-helper/src/main/kotlin/Main.kt
+++ github-action-test-helper/src/main/kotlin/Main.kt
@@ -2,23 +2,22 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.Calendar
 
-fun main(args: Array<String>) {
-    val defaultPath = System.getProperty("user.dir")
-    val rootPath = File(defaultPath).parentFile
-
-    val readMe = File("/home/runner/work/github-action-test-lab/github-action-test-lab/README.md")
+fun main() {
+    val githubBasePath = "/home/runner/work/github-action-test-lab/github-action-test-lab"
+    val readMe = File("$githubBasePath/README.md")
 
     val content = StringBuilder()
-    content.appendLine(readMe.readText())
 
-    val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    content.appendLine(readMe.readText()) // 기존 내용을 그대로 가져옴
 
-    val now = Calendar.getInstance()
 
-    // 한국 시각으로 변경
-    now.add(Calendar.HOUR, 9)
-    
-    content.appendLine("updated at ${simpleDateFormat.format(now.timeInMillis)} - GitHub Actions")
+    val now = Calendar.getInstance().apply {
+        add(Calendar.HOUR, 9) // UTC +9 (한국 시각으로 변경)
+    }
 
-    readMe.writeText(content.toString())
+    val simpleDateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss") // 날짜 포맷
+
+    content.appendLine("updated at ${simpleDateFormat.format(now.timeInMillis)} - GitHub Actions") // 내용 추가
+
+    readMe.writeText(content.toString()) // 파일에 내용을 씀
 }
